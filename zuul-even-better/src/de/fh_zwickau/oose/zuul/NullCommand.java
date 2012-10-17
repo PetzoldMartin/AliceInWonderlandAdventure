cc155e09dcbea71c222600335a01f21e00687ee4
map111ks <map111ks@ba192332-30c4-4e15-8a24-ad2cdf345aae>
2012-10-17 11:52:07 +0000

    projekt erstellt

---
+++ zuul-even-better/src/de/fh_zwickau/oose/zuul/NullCommand.java
@@ -0,0 +1,27 @@
+package de.fh_zwickau.oose.zuul;
+/**
+ * Implementation of the 'help' user command.
+ * 
+ * @author Michael Kolling
+ * @version 1.0 (December 2002)
+ */
+public class NullCommand extends Command
+{
+    
+    /**
+     * Constructor for objects of class HelpCommand
+     */
+    public NullCommand()
+    {
+        // nothing to do
+    }
+    
+    /**
+     * Print out an error message
+     */
+    public boolean execute(Player player)
+    {
+    	System.out.println("I don't understand...(Type \"help\" for help.)");
+        return false;
+    }
+}
