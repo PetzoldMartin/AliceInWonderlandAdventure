23e4d47b14689bd8847bce0a290fd1ea1f3a8367
map111ks <map111ks@ba192332-30c4-4e15-8a24-ad2cdf345aae>
2012-11-14 08:43:24 +0000

    Test Verbesserung Teil 1 von X

--- zuul-even-better/tests/de/fh_zwickau/oose/zuul/PlayerTest.java
+++ zuul-even-better/tests/de/fh_zwickau/oose/zuul/PlayerTest.java
@@ -1,18 +1,27 @@
 package de.fh_zwickau.oose.zuul;
 
 import static org.junit.Assert.*;
+import gameBuilding.LevelCreator;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import GamePlayEnums.PlayerSize;
+
 public class PlayerTest extends TestCase{
 	Player player;
+	private LevelCreator levelcreator;
+	private Room room;
 
 	@Before
 	public void setUp() throws Exception {
 		player = new Player();
+		levelcreator= new LevelCreator();
+		room= levelcreator.getStartRoom();
+		
+		
 	}
 
 	@Test
@@ -21,11 +30,30 @@ public class PlayerTest extends TestCase{
 		
 	}
 
+	@Test
+	public void testGetSize(){
+		assertEquals(PlayerSize.NORMAL,player.getSize());
+	}
 
 	@Test
+	public void testSetSize(){
+		player.setSize(PlayerSize.LITTLE);
+		assertEquals(PlayerSize.LITTLE,player.getSize());
+		player.setSize(PlayerSize.BIG);
+		assertEquals(PlayerSize.BIG,player.getSize());
+		player.setSize(PlayerSize.BIG);
+		assertEquals(PlayerSize.BIG,player.getSize());
+	}
+	@Test
 	public void testWalk() {
+		player.setCurrentRoom(room);
 		player.walk("osten");
-		//Assert.assert
+		
+		assertFalse(levelcreator.getStartRoom().equals(player.getCurrentRoom()));
+		player.walk("westen");
+		assertEquals(levelcreator.getStartRoom(), player.getCurrentRoom());
+		player.walk("Schwachsinn");
+		assertEquals(levelcreator.getStartRoom(), player.getCurrentRoom());
 	}
 
 
